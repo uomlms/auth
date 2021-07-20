e2bde09bcd926bcbab064abd5c33370034c62cb6
OrestisNer <orestis@ep-mail.gr>
2021-07-21 01:19:43 +0300

    When auth service starts, an admin user is created if not exists.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,32 @@
 import { connectDB } from '../config/db';
 import { app } from './app';
+import { User } from './models/user';
+
+const createAdminUser = async () => {
+  if (
+    !process.env.ADMIN_USERNAME ||
+    !process.env.ADMIN_EMAIL ||
+    !process.env.ADMIN_PASSWORD
+  ) {
+    return;
+  }
+
+  const data = {
+    name: process.env.ADMIN_USERNAME,
+    email: process.env.ADMIN_EMAIL,
+    password: process.env.ADMIN_PASSWORD,
+    role: "staff"
+  };
+
+  const adminUser = await User.findOne({ email: data.email });
+  if (!adminUser) {
+    const user = User.build(data);
+    await user.save();
+  }
+}
 
 const start = async () => {
+  createAdminUser();
   if (!process.env.JWT_SECRET) {
     throw new Error('JWT_SECRET must be defined');
   }
--- src/models/user.ts
+++ src/models/user.ts
@@ -33,15 +33,20 @@ interface UserDoc extends mongoose.Document {
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, "Please add a name"]
+    required: true,
   },
   email: {
     type: String,
-    required: [true, "Please add an email"]
+    required: true,
+    unique: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please add a valid email"
+    ]
   },
   password: {
     type: String,
-    required: [true, "Please add a password"]
+    required: true
   },
   role: {
     type: String,
