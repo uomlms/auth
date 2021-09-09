b031d277d3dad890b360a9c681a0a8215514871c
Orestis <orestis@ep-mail.gr>
2021-09-09 12:59:32 +0300

    Remove swagger-jsdoc. Create openapi/docs.json file that describes the endpoints.

--- src/app.ts
+++ src/app.ts
@@ -10,9 +10,8 @@ import 'express-async-errors';
 import dotenv from 'dotenv';
 import { resolve } from "path"
 import cookieSession from 'cookie-session';
-import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
-import docs from '../config/docs.json';
+import docs from '../openapi/docs.json';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -24,8 +23,7 @@ dotenv.config({ path: resolve(__dirname, "../config/config.env") });
 
 const app = express();
 
-const swaggerSpec = swaggerJSDoc(docs);
-app.use('/api/users/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/api/users/docs', swaggerUi.serve, swaggerUi.setup(docs));
 
 // not to have problem with ingress-ngnix
 app.set('trust proxy', true);
--- src/routes/current-user.ts
+++ src/routes/current-user.ts
@@ -3,41 +3,8 @@ import { currentUser } from '@uomlms/common';
 
 const router = express.Router();
 
-/**
- * @swagger
- *
- * /api/users/currentuser:
- *   get:
- *     summary: Returns the current user.
- *     description: Returns the current user. if token not provided or expires will return null
- *     produces:
- *       - application/json
- *     responses:
- *       200:
- *         description: Returns the current user. If JWT is not provided as cookie, will return null
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 currentUser:
- *                   type: object
- *                   properties:
- *                     id:
- *                       type: string
- *                       description: The currennt user's id.
- *                       example: 60a96e2f
- *                     email:
- *                       type: email
- *                       description: The currennt user's email.
- *                       example: test@test.gr
- *                     iat:
- *                       type: integer
- *                       description: JWT Issued At.
- *                       example: 1621717307
- */
 router.get('/api/users/currentuser', currentUser, (req, res) => {
-  // we want to return null instead of undefined in case of not logged in user
+  // it returns null instead of undefined in case of not logged in user
   res.send({ currentUser: req.currentUser || null });
 });
 
